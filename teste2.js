c8b6102b86b0c61a90c1c4a61787c342027e7525
NoelyGangello <noely.gangello@outlook.com>
2022-09-21 01:53:43 -0300

    alteracoes feitas em projeto:
     Changes to be committed:
    	new file:   aprendendogit.js
    	deleted:    arquivonovo.js
    	deleted:    teste2.js
    	deleted:    teste4.js

--- teste2.js
+++
@@ -1,9 +0,0 @@
-var rftrkkskskskksyrf = pessoas
-
-var listaDeNumeros = [
-    1,2,3,4];HTMLAreaElementnghnjhyjngyhjjjjjjjjjjj7y´çj´gff
-    dletlgffolgkotrkhoifgb
-
-    fucn9djifiurjgifjgifigifji =difjidfjirfjirf
-    fijijdifjifjifjiffhyhjygmkjl.ççlçpoç~po~po´~jhgjyuhjyjyjgyjujyjyjytyttyhjnyjhn 
-    jkukukkm,m,m,m,m,m,m,
